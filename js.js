e8fed5aee318d594ab6156b1460650decf0f0926
DaehyeonKim <Daehyeonoff@gmail.com>
2021-08-23 23:42:43 +0900

    20210823

--- js.js
+++
@@ -1,9 +0,0 @@
-const toggleBtn = document.querySelector(".navbar-toogleBtn");
-const menu = document.querySelector(".navbar-menu");
-const link = document.querySelector(".navbar-links");
-
-
-toggleBtn.addEventListener("click", () => {
-    menu.classList.toggle("active");
-    link.classList.toggle("active");
-});
